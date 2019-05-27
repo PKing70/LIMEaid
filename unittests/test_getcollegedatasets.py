97ec951daf6ad05f652781240da9119e5f58223e
unknown <sumanbh@uw.edu>
2019-05-27 01:13:48 -0700

    trying with unittest ramework

--- unittests/test_getcollegedatasets.py
+++ unittests/test_getcollegedatasets.py
@@ -1,15 +1,19 @@
+import unittest
 from model import getcollegedatasets
-import pytest
 
 
-def test_basic_pass():
-    """
-    This is a basic pass test case scenario to make sure coverage banner works
-    """
-    assert(1 == 1)
-	
-def test_getmostrecentcohorts():
-    """
-    This is a placeholder to test get most recent cohorts dataset
-    """
-    assert(1 == 1)
+class TestDataModel(unittest.TestCase):
+	def test_basic_pass(self):
+		"""
+		This is a basic pass test case scenario to make sure coverage banner works
+		"""
+		self.assertEqual(1,1)
+		
+	def test_getmostrecentcohorts(self):
+		"""
+		This is a placeholder to test get most recent cohorts dataset
+		"""
+		self.assertEqual(1,1)
+
+SUITE = unittest.TestLoader().loadTestsFromTestCase(TestDataModel)
+_ = unittest.TextTestRunner().run(SUITE)
