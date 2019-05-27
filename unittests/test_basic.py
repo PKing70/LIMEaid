74218b0f95acec5eda6e87af5a115d12f8ec9bd4
unknown <sumanbh@uw.edu>
2019-05-27 00:13:09 -0700

    more code to check code coverage functionality

--- unittests/test_basic.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def test_basic_pass():
-    """
-    This is a basic pass test case scenario to make sure coverage banner works
-    """
-    assert(1 == 1)
---
+++ unittests/test_getcollegedatasets.py
@@ -0,0 +1,15 @@
+from model import getcollegedatasets
+import pytest
+
+
+def test_basic_pass():
+    """
+    This is a basic pass test case scenario to make sure coverage banner works
+    """
+    assert(1 == 1)
+	
+def test_getmostrecentcohorts():
+    """
+    This is a placeholder to test get most recent cohorts dataset
+    """
+    assert(1 == 1)
