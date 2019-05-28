95e04783828ce38869e287a7466c4577ce9ee3c3
sumanbhagavathula <sumanbh@uw.edu>
2019-05-27 23:45:31 -0700

    fix assert stmt

--- codebase/unittests/test_getcollegedatasets.py
+++ codebase/unittests/test_getcollegedatasets.py
@@ -5,11 +5,11 @@ def test_basic_pass():
 	"""
 	This is a basic pass test case scenario to make sure coverage banner works
 	"""
-	self.assertEqual(1,1)
+	assert(1==1)
 		
 def test_getmostrecentcohorts():
 	"""
 	This is a placeholder to test get most recent cohorts dataset
 	"""
-	self.assertEqual(1,1)
+	assert(1==1)
 
