361bc53ab123ff758335e892c127f9b39658b91a
unknown <sumanbh@uw.edu>
2019-06-01 10:07:42 -0700

    relative path for data dir

--- codebase/model/getcollegedatasets.py
+++ codebase/model/getcollegedatasets.py
@@ -1,17 +1,24 @@
 import pandas as pd
-import os.path as op
+import os
 
 
 def getmostrecentcohorts():
     """
     This function returns the most recent cohorts dataset
     """
-    dataset = pd.read_csv(op.join(".\\data", 'Most-_Recent-_Cohorts-_Scorecard-_Elements.csv'))
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data/Most-_Recent-_Cohorts-_Scorecard-_Elements.csv")
+    with open(path) as f:
+        print(f)
+        dataset = pd.read_csv(f)
     return dataset
 
 def getsalariesbyregion():
     """
     This function returns the salaries by region dataset
-    """
-    dataset = pd.read_csv(op.join(".\\data", 'salaries-by-region.csv'))
+    """ 
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data/salaries-by-region.csv")
+    with open(path) as f:
+        dataset = pd.read_csv(f)
     return dataset
