777c2fd046ca1c772d7862e58611615a9d98bcc1
unknown <sumanbh@uw.edu>
2019-06-01 10:25:39 -0700

    relative path for data dir

--- codebase/model/getcollegedatasets.py
+++ codebase/model/getcollegedatasets.py
@@ -7,7 +7,7 @@ def getmostrecentcohorts():
     This function returns the most recent cohorts dataset
     """
     my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "../data/Most-_Recent-_Cohorts-_Scorecard-_Elements.csv")
+    path = os.path.join(my_path, ".../data/Most-_Recent-_Cohorts-_Scorecard-_Elements.csv")
     with open(path) as f:
         print(f)
         dataset = pd.read_csv(f)
