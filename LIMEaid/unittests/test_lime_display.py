e6ebef374f9b19a655f94855cf23cd2bfc597698
J. Salido <javiers@uw.edu>
2019-06-08 12:16:43 -0700

    Fixed test.

--- LIMEaid/unittests/test_lime_display.py
+++ LIMEaid/unittests/test_lime_display.py
@@ -15,9 +15,4 @@ def test_lime_display():
     x_class = 0
     features = ["A", "B", "C"]
     # Plot dummy data. If plot throws an error, test fails.
-    try:
-        ld.lime_display(data, lime_beta, lime_int, x, x_class,
-                        features)
-        assert(True)
-    except:
-        assert(False)
+    ld.lime_display(data, lime_beta, lime_int, x, x_class, features)
