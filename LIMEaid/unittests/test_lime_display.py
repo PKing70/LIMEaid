6f28adf39a57dd2a8a40f0295fcc702f8f37dba4
J. Salido <javiers@uw.edu>
2019-06-08 10:07:25 -0700

    Test for lime_display v1.

---
+++ LIMEaid/unittests/test_lime_display.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0, '../LIMEaid/view')
+import LIMEdisplay as ld
+import numpy as np
+
+
+def test_lime_display():
+    # Input some dummy data so that lime_display has something to
+    # display.
+    data = np.array([[1, 2, 3, 0], [4, 5, 6, 1], [7, 8, 9, 2]],
+                    np.int32)
+    lime_beta = np.array([1, 1, 0])
+    lime_int = 0.5
+    x = [1.5, 1.5, 1.5]
+    x_class = 0
+    features = ["A", "B", "C"]
+    try:
+        ld.lime_display(data, lime_beta, lime_int, x, x_class, features)
+        assert(True)
+    except:
+        assert(False)
